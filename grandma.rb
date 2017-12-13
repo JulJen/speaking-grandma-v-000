c2b2fce77a168fae286135632308b2bd388ca8a1
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-13 18:28:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 
 
 def speak_to_grandma
-  if 'I LOVE YOU GRANDMA!'
+  if I LOVE YOU GRANDMA!
     puts 'I LOVE YOU TOO PUMPKIN!'
   # elsif 'Hi Nana, how are you?' || 'Hi!'
   #   puts 'HUH?! SPEAK UP, SONNY!'
