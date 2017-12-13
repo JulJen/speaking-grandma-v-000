de8c5e7696fa660086b363b76f0a0170459aa546
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-13 18:35:02 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
     puts 'HUH?! SPEAK UP, SONNY!'
   elsif phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   else
     puts 'HUH?! SPEAK UP, SONNY!'
   end
