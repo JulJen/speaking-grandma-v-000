780ac1c4331f38d6360f46516d53e936a2d70ee9
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-13 18:27:15 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma
     puts 'I LOVE YOU TOO PUMPKIN!'
   elsif 'Hi Nana, how are you?' || 'Hi!'
     puts 'HUH?! SPEAK UP, SONNY!'
-  elsif 'WHAT DID YOU EAT TODAY'
+  elsif 'WHAT DID YOU EAT TODAY' || 'WHAT?'
     puts "NO, NOT SINCE 1938!"
   else
     puts 'HUH?! SPEAK UP, SONNY!'
